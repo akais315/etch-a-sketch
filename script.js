76d77ecbbd70d73e12911753c1dfffb997a6ea94
akais315 <akais315@gmail.com>
2022-06-26 22:29:43 +0200

    Add 'Reset grid' button with grid size input

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function createGrid(rows, columns){
-    const body = document.querySelector('#body');
+function createGrid(rows){
     const grid = document.createElement('div');
     body.appendChild(grid);
     grid.id = "grid";
@@ -9,20 +8,45 @@ function createGrid(rows, columns){
         gridSelector.appendChild(newDiv);
         newDiv.className = "rows";
         newDiv.id = "r"+i;
-        for (let j = 0; j < columns; j++){
+        for (let j = 0; j < rows; j++){
             let newCol = document.createElement('div');
             newDiv.appendChild(newCol);
             newCol.className = "gridCell";
             newCol.id = "c"+j;
         }
     }
+    addListener();
+}
+
+function addListener(){
+    const cellGrid = Array.from(document.querySelectorAll('.gridCell'));
+    cellGrid.forEach(cell => {cell.addEventListener('mouseenter', hover);});
 }
 
 function hover(e){
     e.target.classList.add("hovered");
 }
 
-createGrid(16,16);
+function deleteGrid(){
+    const grid = document.getElementById("grid");
+    grid.remove();
+}
 
-const cellGrid = Array.from(document.querySelectorAll('.gridCell'));
-cellGrid.forEach(cell => {cell.addEventListener('mouseenter', hover);});    
+function resetGrid(){
+    let rows = 0;
+    while(rows>100 || rows <= 0){
+        rows = window.prompt("Input grid size(between 1 and 100) :", 16);
+    }
+    deleteGrid();
+    createGrid(rows);
+}
+
+const body = document.querySelector('#body');
+const button = document.createElement('button');
+button.innerHTML = "Reset grid";
+body.appendChild(button);
+
+button.addEventListener('click', resetGrid);
+
+createGrid(16);
+
